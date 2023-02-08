0db2429879dd9c9d1a0f7c115bf7134853febe05
Joseluis788 <43463998+Joseluis788@users.noreply.github.com>
2023-02-08 14:43:39 +0100

    a

---
+++ DIW/index.js
@@ -0,0 +1,10 @@
+var CarouselWidth = $('.carousel-inner').scrollWidth;
+var cardWidth = $('.carousel-item').width();
+
+var scrollPosition = 0;
+
+$('.carousel-control-next').on('click', function(){
+    console.log("next");
+    scrollPosition = scrollPosition + cardWidth;
+    $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600);
+});
